f24fff9b898e35e3ec218969ba020b8d4502acfa
tkmagesh <tkmagesh77@gmail.com>
2014-11-08 16:57:56 +0530

    modularization

--- Day-01/03-NodeWebServer/server.js
+++ Day-01/03-NodeWebServer/server.js
@@ -2,6 +2,7 @@
 psuedocode
 ==========
 parse the url
+extract the data
 if the request is for a static resource [html, js, img, jpg, png, css, txt]
     if resource exists
         server the resource
@@ -21,22 +22,75 @@ else
 */
 var http = require("http"),
     path = require("path"),
+    qs = require("querystring"),
+    url = require("url"),
+    calculator = require("./calculator"),
     fs = require("fs");
-var server = http.createServer(function(req,res){
-    var resourcePath = path.join(__dirname, req.url);
-    if (fs.existsSync(resourcePath)){
-        var readStream = fs.createReadStream(resourcePath, {encoding : "utf8"});
-        /*readStream.on("data", function(chunk){
-            res.write(chunk);
-        });
-        readStream.on("end", function(){
-            res.end();
-        });*/
-        readStream.pipe(res);
-    } else {
-        res.statusCode = 404;
-        res.end();
+
+
+
+function serveStatic(req,res,next){
+    var staticResourceExtns =  [".html", ".js", ".img", ".jpg", ".png", ".css", ".txt",".ico"];
+
+    function isStaticResource(resourceName){
+        return staticResourceExtns.indexOf(path.extname(resourceName)) != -1;
     }
+    
+    var resourcePath = path.join(__dirname, req.url.pathname);
+    if (isStaticResource(resourcePath){
+        if (fs.existsSync(resourcePath)){
+            var readStream = fs.createReadStream(resourcePath, {encoding : "utf8"});
+            readStream.pipe(res);
+        } else {
+            res.statusCode = 404;
+            res.end();
+        }
+    } else 
+    {
+        next();
+    }
+}
+
+function extractData(req,res,next){
+    var qsObj = qs.parse(req.url.query);
+    req.queryData = qsObj;
+    if (req.method === "POST"){
+            var data = '';
+            req.on("data",function(chunk){
+                data += chunk;
+            });
+            req.on("end", function(){
+                var qsObj = qs.parse(data);
+                req.bodyData = qsObj;
+                next();
+            });
+        }
+    else {
+        next();
+    }
+}
+
+function handleCalculatorRequest(req,res,next){
+    if (req.url.pathname === "/calculator"){
+        var reqData;
+        if (req.method === "GET"){
+            reqData = req.queryData;
+        } else {
+            reqData = req.bodyData;
+        }
+        var operator = reqData.operator,
+        number1 = parseInt(reqData.number1, 10),
+        number2 = parseInt(reqData.number2, 10);
+        var result = calculator[operator](number1, number2);
+        res.write(result.toString());
+        res.end();  
+    } else {
+        next();    
+    }
+}
+var server = http.createServer(function(req,res){
+    
+    
 });
 server.listen(9090);
 console.log("server listening on port 9090..!");
