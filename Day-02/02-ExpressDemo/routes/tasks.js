4705ea698fecf552d8d590842a6aa4ae22343f85
tkmagesh <tkmagesh77@gmail.com>
2014-11-09 12:12:32 +0530

    Adding a new task - seed

--- Day-02/02-ExpressDemo/routes/tasks.js
+++ Day-02/02-ExpressDemo/routes/tasks.js
@@ -1,9 +1,40 @@
 var express = require("express");
 var routes = express.Router();
 
+var taskList = [
+    'Learn JavaScript',
+    'Explore Angular.js',
+    'Master Node.js'
+];
+
+//index - list the tasks
 routes.get('/', function(req,res){
-    res.write("<h1>Task Manager</h1>");
-    res.end();
+    var viewData = {
+        tasks : taskList
+    };
+    res.render('tasks/index',   viewData);
 });
+
+
+
+//add[get] - screen to add a new task
+routes.get('/new',function(req,res){
+    res.render('tasks/new');
+});
+
+//add[post] - save the new task
+//            - redirect to index
+routes.post('/new',function(req,res){
+    console.log(req.body);
+    res.redirect('/tasks');
+});
+
+//remove[get] - show a confirm for removal
+
+//remove[post] - remove the completed tasks
+//            - redirect to index
+
+//toggle/id[get] - toggle task status
+//            - redirect to index
 
 module.exports = routes;
